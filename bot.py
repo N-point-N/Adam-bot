252b267a43af2eacf4a8e353c49ea33db04fba23
N-point-N <pashales97@gmail.com>
2018-03-04 01:24:45 +0300

    Test commit

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 import config
 import telebot
+import logging
 
 bot = telebot.TeleBot(config.token);
 
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text + "GITtesting")
+
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    try:
+        bot.polling(none_stop=True)
+    except Exception as err:
+        logging.error(err)
+        print("Internet error")
