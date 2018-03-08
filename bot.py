555e6b79079de70a91b027fdfc049fa34a0557bf
Lopaste <pashales97@gmail.com>
2018-03-08 15:04:25 +0300

    Second commit

--- bot.py
+++ bot.py
@@ -2,9 +2,16 @@ import config
 import telebot
 import logging
 
-bot = telebot.TeleBot(config.token);
+bot = telebot.TeleBot(config.token)
+commands = ['start','spacite','help']
 
-
+@bot.message_handler(commands=['start'])
+def starting_func(message):
+    bot.send_message(message.chat.id, 'KOLOBOK')
+@bot.message_handler(commands=['help'])
+def print_commandslist(message):
+    for i in commands:
+        bot.send_message(message.chat.id,i)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text + "GITtesting")
