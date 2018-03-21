40b55c1b9bd5d4956fb5d75b54c445dd25cfcf33
Lopaste <pashales97@gmail.com>
2018-03-21 22:59:41 +0300

    Added precipitation string

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
-import logging
 from datetime import datetime
 from pytz import timezone
 from timezonefinder import  TimezoneFinder
@@ -33,28 +32,38 @@ def get_weather(message):
 
 @bot.message_handler(func=(lambda _ : weather_c), content_types=["location"])
 def repeat_all_messages(message):
-    template_en =['City: ','Current weather: ','temperature: ','pressure: ','wind: ', 'sunrise: ','sunset: ']
-    template_ru = ['Город: ','Текущая погода: ','температура: ','давление: ','ветер: ','восход: ','закат: ']
+    #=======================================DESCRIPTION====================================
+    template_short_en  = ['Current weather: ','temperature: ','precipitation expected: ','wind: ']
+    template_short_ru = ['Текущая погода: ', 'температура: ', 'вероятность осадков: ', 'ветер: ']
+    template_en =['City: ','Current weather: ','temperature: ','pressure: ','precipitation:','wind: ', 'sunrise: ','sunset: ']
+    template_ru = ['Город: ','Текущая погода: ','температура: ','давление: ','осадки: ','ветер: ','восход: ','закат: ']
     template_dim_ru = [' мм',' м/с']
     template_dim_en =[' mm',' m/sec']
+    #========================================INIT TEXT======================================
+    template = template_ru
+    template_short = template_short_ru
+    template_dim = template_dim_ru
+    #========================================START HANDLER===================================
     tf = TimezoneFinder()
     tt =timezone(tf.timezone_at(lng=message.location.longitude,lat=message.location.latitude))
     obs = owm.weather_at_coords(message.location.latitude, message.location.longitude)
     w = obs.get_weather()
     sunrize = datetime.fromtimestamp(w.get_sunrise_time()).astimezone(tt)
     sunzet = datetime.fromtimestamp(w.get_sunset_time()).astimezone(tt)
-    temp = w.get_temperature(unit='celsius')
-    template = template_ru
-    template_dim = template_dim_ru
-    text = template[0] + obs.get_location().get_name() + '\n' \
-              + template[1] +'\n' \
-              + w.get_detailed_status() + '\n' \
-              + template[2] + str(temp['temp']) + " °C" + '\n' \
-              + template[3] + str(round(w.get_pressure()['press']/1.333224)) + template_dim[0]+'\n' \
-              + template[4] + str(w.get_wind()['speed']) + template_dim[1] +'\n' \
-              + template[5] + sunrize.strftime('%H:%M:%S') + '\n' \
-              + template[6] + sunzet.strftime('%H:%M:%S')
-    bot.send_message(message.chat.id, text)
+    text_short = template_short[0] + '\n' \
+          + w.get_detailed_status() + '\n' \
+          +template_short[1] + str(w.get_temperature(unit='celsius')['temp']) +"°C"+ '\n' \
+          +template_short[2] + str(w.get_clouds()) + ' %\n'\
+          +template_short[3] + str(w.get_wind()['speed']) + template_dim[1] + '\n'
+    #text = template[0] + obs.get_location().get_name() + '\n' \
+    #          + template[1] +'\n' \
+    #          + w.get_detailed_status() + '\n' \
+    #          + template[2] + str(w.get_temperature(unit='celsius')['temp']) + " °C" + '\n' \
+    #          + template[3] + str(round(w.get_pressure()['press']/1.333224)) + template_dim[0]+'\n' \
+    #          + template[4] + str(w.get_wind()['speed']) + template_dim[1] +'\n' \
+    #          + template[5] + sunrize.strftime('%H:%M:%S') + '\n' \
+    #          + template[6] + sunzet.strftime('%H:%M:%S')
+    bot.send_message(message.chat.id, text_short)
     global weather_c
     weather_c = False
 
